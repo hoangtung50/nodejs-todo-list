539c15522a70fdf2432cc7498742a3070830d107
Tung Nguyen <tungnh90@gmail.com>
2020-04-17 10:36:43 +0700

    update home

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.send('Hi!')
+})
+
 // Backend API
 app.use('/api', api);
 
